b3d3e065ddaed639f2486688158c94f6f80cd8d5
gnuh96 <ntuanhung.96@gmail.com>
2023-01-22 02:59:17 +0100

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import "./App.scss";
+import React, { Suspense } from 'react';
+import { BrowserRouter, Link, Redirect, Route, Switch } from 'react-router-dom';
+import './App.scss';
+import NotFound from './components/NotFound';
+
+const Photo = React.lazy(() => import('./features/Photo'));
 
 function App() {
-  return <div className="App"></div>;
+    return (
+        <div className="photo-app">
+            <Suspense fallback={<div>Loading...</div>}>
+                <BrowserRouter>
+                    <ul>
+                        <li>
+                            <Link to="/photos">Go to photo page</Link>
+                        </li>
+                        <li>
+                            <Link to="/photos/add">Go to add new photo page</Link>
+                        </li>
+                        <li>
+                            <Link to="/photos/123">Go to edit photo page</Link>
+                        </li>
+                    </ul>
+                    <Switch>
+                        <Redirect exact from="/" to="/photos" />
+                        <Route path="/photos" component={Photo} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </BrowserRouter>
+            </Suspense>
+        </div>
+    );
 }
 
 export default App;
